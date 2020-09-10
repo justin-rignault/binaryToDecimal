10b1e89e604c470322f2d26d3a08528780ce4412
justin-rignault <justinrignault@gmail.com>
2020-09-10 14:17:08 -0300

    page layout/html done

---
+++ src/components/input.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Input extends React.Component {
+    render() {
+        return (
+            <div>
+                <input></input>
+                <button>Convert</button>
+                <p></p>
+            </div>
+        )
+    }
+}
+
+export default Input
