b740b73d3c39c87882e8ef87e2ca56c18981cd52
justin-rignault <justinrignault@gmail.com>
2020-09-11 18:29:56 -0300

    Updates

---
+++ precache-manifest.66aaf1934aac2c55dcdbb68309a02e51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed9de5653eedc9d8cb049d44f602670c",
+    "url": "/binaryToDecimal/index.html"
+  },
+  {
+    "revision": "a520624df1f6a0ea6c6b",
+    "url": "/binaryToDecimal/static/css/main.0f85f696.chunk.css"
+  },
+  {
+    "revision": "afc4a74b301f6bc10c23",
+    "url": "/binaryToDecimal/static/js/2.b4cd3bbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/binaryToDecimal/static/js/2.b4cd3bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a520624df1f6a0ea6c6b",
+    "url": "/binaryToDecimal/static/js/main.1a2faad3.chunk.js"
+  },
+  {
+    "revision": "40cb74014b3c42cfa092",
+    "url": "/binaryToDecimal/static/js/runtime-main.34213142.js"
+  }
+]);
